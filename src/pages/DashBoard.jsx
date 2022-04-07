5625c5b3a8305673f727a68e9f5908dfee6b6566
Garikoitz Herrero Vargas <gariherrero@gmail.com>
2022-04-07 15:32:47 +0200

    update

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -10,22 +10,69 @@ import {
   Slider,
   TextField,
 } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CardImage, CardText } from '../components/card';
 import { NavBar } from '../components/navBar/Navbar';
 import { FetchInitialEstate } from '../redux/dbRedux/actions';
 import { CardTextSelected } from '../components/card/CardTextInfo';
 import { SearchBar } from '../components/search/SearchBar';
+import axios from 'axios';
+import { queryProperties } from '../helper/query';
 
 export const DashBoard = () => {
   const data = useSelector((state) => state.reducerRealEstate);
+  const [value, setValue] = useState([40000, 150000]);
+  const [inputValue, setinputValue] = useState({
+    bedroom1: false,
+    bedroom2: false,
+    bedroom3: false,
+    bedroom4: false,
+    bathroom1: false,
+    bathroom2: false,
+    bathroom3: false,
+    pet: false,
+    lift: false,
+    garden: false,
+    air: false,
+    pool: false,
+    terrace: false,
+  });
+  const handleChangeAll = ({ target: { name, checked } }) => {
+    console.log(checked);
+    setinputValue({
+      ...inputValue,
+      [name]: checked,
+    });
+  };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     (async () => {
-      console.log(data);
+      try {
+        let allValues = [];
+        Object.keys(inputValue).forEach((element) => {
+          if (inputValue[element]) {
+            allValues.push(`${element}=${inputValue[element]}&`);
+          }
+        });
+
+        const response = await axios.get(
+          `http://localhost:3000/properties?${allValues}`
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [inputValue]);
+
+  useEffect(() => {
+    (async () => {
       dispatch(await FetchInitialEstate());
     })();
   }, [dispatch]);
@@ -57,32 +104,32 @@ export const DashBoard = () => {
                       <FormControlLabel
                         control={<Checkbox />}
                         label="1"
-                        name="1"
-                        onClick={handleClick}
+                        name="bedroom1"
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
                       <FormControlLabel
                         control={<Checkbox />}
                         label="2"
-                        name="2"
-                        onClick={handleClick}
+                        name="bedroom2"
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
                       <FormControlLabel
                         control={<Checkbox />}
                         label="3"
-                        name="3"
-                        onClick={handleClick}
+                        name="bedroom3"
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
                       <FormControlLabel
                         control={<Checkbox />}
                         label="4"
-                        name="4"
-                        onClick={handleClick}
+                        name="bedroom4"
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                   </Grid>
@@ -94,24 +141,24 @@ export const DashBoard = () => {
                       <FormControlLabel
                         control={<Checkbox />}
                         label="1"
-                        name="1"
-                        onClick={handleClick}
+                        name="bathroom1"
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
                       <FormControlLabel
                         control={<Checkbox />}
                         label="2"
-                        name="2"
-                        onClick={handleClick}
+                        name="bathroom2"
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
                       <FormControlLabel
                         control={<Checkbox />}
                         label="3"
-                        name="3"
-                        onClick={handleClick}
+                        name="bathroom3"
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                   </Grid>
@@ -123,32 +170,32 @@ export const DashBoard = () => {
                   </Typography>
                   <Grid container spacing={2}>
                     <Grid item xs={6} md={6} xl={6}>
-                      <Grid container spacing={1}>
+                      <Grid container spacing={7}>
                         <Grid item xs={6} md={6} xl={6}>
                           <TextField
                             id="outlined-basic"
-                            label="Min. "
                             name="minPrice"
                             variant="outlined"
+                            value={`€ ${value[0]} `}
+                            sx={{ width: '105px' }}
                           />
                         </Grid>
                         <Grid item xs={6} md={6} xl={6}>
                           <TextField
                             id="outlined-basic"
-                            label="Max."
                             name="maxPrice"
                             variant="outlined"
+                            value={`€ ${value[1]} `}
+                            sx={{ width: '105px' }}
                           />
                         </Grid>
                       </Grid>
                       <Slider
-                        aria-label="Price Range"
-                        name="price"
-                        defaultValue={200000}
+                        getAriaLabel={() => 'Temperature range'}
+                        value={value}
+                        onChange={handleChange}
                         valueLabelDisplay="auto"
-                        step={1000}
-                        marks
-                        min={40000}
+                        min={30000}
                         max={1000000}
                         sx={{ pt: 6 }}
                       />
@@ -163,7 +210,7 @@ export const DashBoard = () => {
                         control={<Checkbox />}
                         label="Pets Allowed"
                         name="pets"
-                        onClick={handleClick}
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
@@ -171,7 +218,7 @@ export const DashBoard = () => {
                         control={<Checkbox />}
                         label="Lift"
                         name="lift"
-                        onClick={handleClick}
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
@@ -179,7 +226,7 @@ export const DashBoard = () => {
                         control={<Checkbox />}
                         label="Garden"
                         name="garden"
-                        onClick={handleClick}
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
@@ -187,7 +234,7 @@ export const DashBoard = () => {
                         control={<Checkbox />}
                         label="Air Conditioning"
                         name="air"
-                        onClick={handleClick}
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
@@ -195,7 +242,7 @@ export const DashBoard = () => {
                         control={<Checkbox />}
                         label="Swimming Pool"
                         name="pool"
-                        onClick={handleClick}
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                     <Grid item xs={6} md={6} xl={6}>
@@ -203,7 +250,7 @@ export const DashBoard = () => {
                         control={<Checkbox />}
                         label="Terrace"
                         name="terrace"
-                        onClick={handleClick}
+                        onClick={handleChangeAll}
                       />
                     </Grid>
                   </Grid>
